d486cf4d471f335161ca74fa9165fbf101f09dae
Vinícius <viniciuscmgo@gmail.com>
2018-01-06 19:21:17 -0200

    added option to set init values of lstm weights

--- model.py
+++ model.py
@@ -19,7 +19,11 @@ class Model:
 	data_sections = data_size/batch_size
 	epochs = 7
 	learning_rate = 0.015
+	init_minval_lstm = -0.08
+	init_maxval_lstm = 0.08
 	######################
+
+	vars_dict = {}
 
 	encoder_input_ids = tf.placeholder(shape=[None,None], dtype=tf.int32, name='encoder_inputs')
 	encoder_sequence_length = tf.placeholder(shape=[None], dtype=tf.int32, name='encoder_sequence_length')
@@ -36,9 +40,9 @@ class Model:
 	if num_layers > 1:
 		stacked_encoder = []
 		for layer in range(num_layers):
-			stacked_encoder.append(tf.nn.rnn_cell.LSTMCell(num_units = hidden_units, state_is_tuple=True))
+			stacked_encoder.append(tf.nn.rnn_cell.LSTMCell(num_units = hidden_units, state_is_tuple=True, initializer=tf.random_uniform_initializer(init_minval_lstm, init_maxval_lstm)))
 		encoder_cell = tf.nn.rnn_cell.MultiRNNCell(stacked_encoder, state_is_tuple=True)
-	else: encoder_cell = tf.nn.rnn_cell.MultiRNNCell(tf.nn.rnn_cell.LSTMCell(num_units = hidden_units, state_is_tuple=True), state_is_tuple=True)
+	else: encoder_cell = tf.nn.rnn_cell.LSTMCell(num_units = hidden_units, state_is_tuple=True, initializer=tf.random_uniform_initializer(init_minval_lstm, init_maxval_lstm))
 
 	encoder_outputs, encoder_final_state = tf.nn.dynamic_rnn(
 		cell=encoder_cell,
@@ -58,9 +62,9 @@ class Model:
 	if num_layers > 1:
 		stacked_decoder = []
 		for layer in range(num_layers):
-			stacked_decoder.append(tf.nn.rnn_cell.LSTMCell(num_units = hidden_units, state_is_tuple=True))
+			stacked_decoder.append(tf.nn.rnn_cell.LSTMCell(num_units = hidden_units, state_is_tuple=True, initializer=tf.random_uniform_initializer(init_minval_lstm, init_maxval_lstm)))
 		decoder_cell = tf.nn.rnn_cell.MultiRNNCell(stacked_decoder, state_is_tuple=True)
-	else: decoder_cell = tf.nn.rnn_cell.MultiRNNCell(tf.nn.rnn_cell.LSTMCell(num_units = hidden_units, state_is_tuple=True), state_is_tuple=True)
+	else: decoder_cell = tf.nn.rnn_cell.LSTMCell(num_units = hidden_units, state_is_tuple=True, initializer=tf.random_uniform_initializer(init_minval_lstm, init_maxval_lstm))
 
 
 	decoder_outputs, decoder_final_state = tf.nn.dynamic_rnn(
@@ -88,7 +92,14 @@ class Model:
 	def __init__(self):
 
 		self.x, self.enc_seq_length, self.y, self.target, self.dec_seq_length = build_matrices(self.max_time)
+
 		self.prcss_chat = ProcessChat()
+
+		self.sess = tf.Session()
+		self.sess.run(tf.global_variables_initializer())
+
+		for v in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='decoder'):
+			self.vars_dict[v.name] = v
 
 	def train(self):
 
@@ -97,6 +108,7 @@ class Model:
 		target = self.target
 		enc_seq_length = self.enc_seq_length
 		dec_seq_length = self.dec_seq_length
+		sess = self.sess
 
 		##### batch 0########
 
@@ -122,6 +134,9 @@ class Model:
 
 	def inference(self, chat_input):
 
+		#put option: with feed_previous or without
+		#optimize feed_previous (sem gambiarra)
+
 		decoder_logits = self.decoder_logits
 
 		input_x = np.transpose(np.matrix(chat_input))
@@ -138,7 +153,7 @@ class Model:
 			decoder_prediction = tf.argmax(decoder_probabilites, 2) #[max-time,batch-size]
 
 			prediction = sess.run(decoder_prediction, feed_dict={encoder_input_ids: input_x, 
-					decoder_input_ids: input_y, 
+					decoder_input_ids: matrix_y, 
 					encoder_sequence_length: enc_seq_length,
 					decoder_sequence_length: dec_seq_length})
 
@@ -147,9 +162,38 @@ class Model:
 
 			matrix_y = np.concatenate((np.matrix([1]), prediction),)
 
+'''
+	def init_lstm_weights(self, minval, maxval): #use this logic if lstmcell initializer be deprecated or using other cell types
+
+		vars_dict = self.vars_dict
+		hidden_units = self.hidden_units
+		embedding_size = self.embedding_size
+		var1 = (tf.Variable(tf.random_uniform([(hidden_units+embedding_size), 4*hidden_units], minval, maxval), dtype=tf.float32))
+		var2 = (tf.Variable(tf.random_uniform([(hidden_units*2), 4*hidden_units], minval, maxval), dtype=tf.float32))
+		self.sess.run(var1.initializer)
+		self.sess.run(var2.initializer)
+
+		self.sess.run(vars_dict['decoder/multi_rnn_cell/cell_0/lstm_cell/kernel:0'].assign(
+			var1
+			))
+
+		self.sess.run(vars_dict['decoder/multi_rnn_cell/cell_1/lstm_cell/kernel:0'].assign(
+			var2
+			))
+'''
 
 model = Model()
 
+for v in model.vars_dict:
+	print(v)
+
+print(model.sess.run(model.vars_dict['decoder/multi_rnn_cell/cell_0/lstm_cell/kernel:0']))
+print()
+print(model.sess.run(model.vars_dict['decoder/multi_rnn_cell/cell_0/lstm_cell/bias:0']))
+print()
+print(model.sess.run(model.vars_dict['decoder/multi_rnn_cell/cell_1/lstm_cell/kernel:0']))
+print()
+print(model.sess.run(model.vars_dict['decoder/multi_rnn_cell/cell_1/lstm_cell/bias:0']))
 
 '''
 print(model.x[:,:10])
